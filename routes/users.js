28fa963d9453bdbe083c8a32ae8a1ad41cc97402
clurts <clu@rts.dk>
2017-07-01 20:36:54 +0200

    complete user get post update delete

--- routes/users.js
+++ routes/users.js
@@ -17,32 +17,61 @@ module.exports = function (app) {
         });
     });
 
+    app.get('/users/:username', function (req, res, next) {
+        
+        let user = new User();
+        user.getOne(req.params.username, function (err, result) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            res.send(200, result);
+        });
+    });
+
     app.post('/users', function (req, res, next) {
         let user = new User();
         user.create(req.params, function (err, result) {
             if (err) {
                 console.error(err.message);
-                res.send(400);
+                if (err.message.startsWith('Duplicate')) {
+                    res.send(409);
+                } else {
+                    res.send(400); 
+                }
                 return;
             }
             req.params.id = result.insertId;
-            res.send(201, req.params);
+            res.send(201, result);
         });
     });
 
-    app.get('/users/:username', function (req, res, next) {
-        res.send(200, req.params);
-    });
-
     app.put('/users/:username', function (req, res, next) {
-        res.send(405);
+        console.log(req.params);
+        let user = new User();
+        user.update(req.params, function (err, result) {
+            if (err) {
+                res.send(405); 
+                return;
+            }
+            req.params.id = result.insertId;
+            res.send(201, result);
+        });
     });
 
     app.patch('/users/:username', function (req, res, next) {
         res.send(405);
     });
 
-    app.del('/users/:username', function (req, res, next) {
-        res.send(405);
+    app.del('/users/:id', function (req, res, next) {
+        let user = new User();
+        user.del(req.params.id, function (err, result) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            res.send(204);
+            return next();
+        }); 
     });
 };
--- services/users.js
+++ services/users.js
@@ -5,7 +5,7 @@ const mysql = require(path.join(__dirname, '..', 'config', 'mysql'));
 const db = mysql.connect();
 
 class User {
-    getAll(next) {
+    getAll(next){
         db.execute('SELECT id, username, email, password FROM users', function (err, rows) {
             if (err) {
                 return next(err);
@@ -13,21 +13,34 @@ class User {
             next(null,rows);
         });
     }
-
     getOne(username, next) {
-        db.execute('SELECT id, username, email, password FROM users WHERE username = ?', [username], function (err, rows) {
+        db.execute('SELECT id, fullname, email, username,password FROM users WHERE username = ?', [username], function (err, rows) {
             if (err) {
                 return next(err);
             }
+            console.log(rows);
             next(null, rows);
         });
     }
     create(values, next) {
-        db.execute('INSERT INTO users SET fullname = ?, email = ?, username = ?, password = ?', 
+        db.execute('INSERT INTO users SET fullname = ?, email = ?, username = ?, password = ?',
                     [values.fullname, values.email, values.username, values.password], function(err, row) {
+            if (err) {
+                return next(err);
+            }
+            next(null, row);
+        });
+    }
+    update(values, next) {
+        db.execute('UPDATE users SET fullname = ?, email = ?, username = ?, password = ? WHERE username = ?', 
+                  [values.fullname, values.email, values.username, values.password], function(err, row) {
             if (err) return next(err);
             next(null, row);
         });
+    }
+    del(values, next) {
+        db.execute('DELETE FROM users WHERE ID = ?', [values]);
+        next();
     }
 }
 module.exports = User;
